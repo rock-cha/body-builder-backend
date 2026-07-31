3eec02bd5a2533f40d4a7d491a5fd87a65bb0003
chandhuru <chandhurukt3@gmail.com>
2026-07-31 18:45:49 +0530

    Added Nodemailer backend quote route

--- server.js
+++ server.js
@@ -1,18 +1,21 @@
-import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import quoteRoutes from './routes/quoteRoutes.js';
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+import quoteRoutes from "./routes/quoteRoutes.js";
+import reviewRoutes from "./routes/reviewRoutes.js";
+
 
 dotenv.config();
 
 const app = express();
 
-// Middlewares
 app.use(cors());
 app.use(express.json());
 
-// Routes
-app.use('/api', quoteRoutes);
+// API Routes
+app.use("/api/quote", quoteRoutes);
+app.use("/api/reviews", reviewRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
